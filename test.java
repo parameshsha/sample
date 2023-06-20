a1629f93ce708f5970cb8c4dddc23b5fdd54458c
param <paramesh@gmail.com>
2023-06-20 23:21:01 +0530

    added test java file

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test1 {
+    public static void main(string[] args) {
+        string hungry = "yes";
+        if (hungry == "yes")
+        system.out.println("eat something....");
+        else{
+            system.out.println("do work....");
+        }
+    }
+
+}
